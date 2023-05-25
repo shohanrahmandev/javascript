d39ead4e551a66fbf619c8733afeccd8a30375ac
SHOHAN <shohanurrahman2334@gmail.com>
2023-05-25 14:05:18 +0600

    add comment external js

---
+++ day 2/index.js
@@ -0,0 +1,6 @@
+alert("I am Interested javascript");
+
+//document.write("This is my <br>");
+
+document.write("I am Shohanur Rahman.<br> ");
+document.write("I am Student of Northden Univesity of Bangladesh");
