ef95d3c6ac0d94e09fa8770c0c5c786fc5747ad2
SHOHAN <shohanurrahman2334@gmail.com>
2023-05-28 21:21:39 +0600

    add full name lenght

--- day 3/index.js
+++ day 3/index.js
@@ -4,4 +4,8 @@ document.write(text1);
 var text2 = prompt("Enter the Last Name :");
 document.write(text2);
 
-document.write(text1 + text2);
+var text = text1 + text2;
+
+//document.write(text1 + text2);
+
+document.write("<br>The name of len :" + text.length);
